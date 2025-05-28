7ea5cfce7558c818bcdff7b75e31a721f6c0f62c
weimantian <32875346+weimantian@users.noreply.github.com>
2025-05-28 23:10:37 +0800

    提交

--- 21-前缀和算法.py
+++ 21-前缀和算法.py
@@ -4,12 +4,18 @@
 def finalist(recodes, minLen, maxLen, baseLine):
     n = len(recodes)
     prefix_sum = [0] * (n + 1)  # prefix_sum[i] 表示前recodes中前0到i - 1个元素的和
-    for i in range(1, n):
+    for i in range(1, n + 1):
         prefix_sum[i] = prefix_sum[i - 1] + recodes[i - 1]
+    print(prefix_sum)
     res = 0
     for start in range(n):
         for end in range(minLen + start - 1, maxLen + 1):
-            if (prefix_sum[end] - prefix_sum[start]) / (end - start) >= baseLine:
+            length = end - start + 1
+            sum = prefix_sum[end + 1] - prefix_sum[start]
+            avg = sum / length
+            if avg >= baseLine:
+                print(recodes[start: end + 1])
+                print(avg)
                 res += 1
-    
     return res
+print(finalist([1,2,3,4,5,6], 3, 5, 2))
